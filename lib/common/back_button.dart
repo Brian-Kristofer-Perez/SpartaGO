425be59a29f3e9ab0540c559ca4ecbc3011323d6
Nightfall3594 <briankristoferperez28@gmail.com>
2025-10-06 21:10:04 +0800

    feat: add back_button

---
+++ lib/common/back_button.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CustomBackButton extends StatelessWidget {
+
+  const CustomBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      onPressed: () { Navigator.pop(context); },
+      icon: const Icon(Icons.arrow_back),
+      label: const Text('Back'),
+    );
+  }
+}
